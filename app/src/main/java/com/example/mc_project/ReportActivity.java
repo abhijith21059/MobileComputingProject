19b33171b43203e3527c76573894c3f1f73c42dd
Nehalc26 <nehal.chourasia699@gmail.com>
2022-03-23 12:50:06 +0530

    added image capturing

--- app/src/main/java/com/example/mc_project/ReportActivity.java
+++ app/src/main/java/com/example/mc_project/ReportActivity.java
@@ -2,7 +2,10 @@ package com.example.mc_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ReportActivity extends AppCompatActivity {
@@ -11,7 +14,20 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
-        TextView hello = findViewById(R.id.tvReport);
-        hello.setText("IN REPORTS");
+//        TextView hello = findViewById(R.id.tvReport);
+//        hello.setText("IN REPORTS");
+//        getSupportFragmentManager().beginTransaction().replace(R.id.ReportFragment, new ListReportFragment()).commit();
+        Button add = findViewById(R.id.buttonAdd);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ReportActivity.this, CaptureActivity.class);
+                startActivity(intent);
+//                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.ListReportFragment, new CaptureImageFragment(), "NewFragmentTag").addToBackStack(null).commit();
+
+            }
+        });
+
+
     }
 }
